c8a522489e316dbadffd9d87d33a742234b709ce
Michel Moreira <michel.moreira@academico.ifpb.edu.br>
2021-03-17 10:37:53 +0000

    Questões pares.

---
+++ questao_04.py
@@ -0,0 +1,8 @@
+'''4) Escreva uma função que receba a base e a altura de um triângulo e retorne sua área (A = (base x altura)/2).'''
+
+def área_triângulo(base,altura):
+  return (base * altura)/2
+
+
+print(área_triângulo(6, 9))
+print(área_triângulo(5, 8))
